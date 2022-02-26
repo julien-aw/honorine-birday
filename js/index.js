bd477c5e6959bd287abf2de231d2148da3627afd
julien awon'ga <julienawon@gmail.com>
2022-02-26 19:16:37 +0100

    deuxieme version

--- js/index.js
+++ js/index.js
@@ -9,8 +9,8 @@
 $("#login-button").click(function(event) {
     var userName = document.getElementById("userName").value;
     var pwd = document.getElementById("pwd").value;
-    //修改密码请改此处
-    if (userName == "123" && pwd == "123") {
+    //Change password please change here
+    if (userName == "Honorine" && pwd == "Honorine") {
         event.preventDefault();
         $("form").fadeOut(500);
         $(".wrapper").addClass("form-success");
